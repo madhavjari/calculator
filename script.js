0fc1d08a65be64b986333bbca9dcb43d69432b42
madhavjari <213913746+madhavjari@users.noreply.github.com>
2025-07-31 18:55:16 +0530

    0-9 button display, started with multiply

--- script.js
+++ script.js
@@ -27,6 +27,76 @@ function floatInput(str){
     }while (true);
 }
 
+let strNumber = '';
+let parseNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
+let calc = document.querySelector('.calc');
+let dis = document.createElement('div');
+dis.classList.add('display');
+dis.textContent = parseNumber;
+calc.insertBefore(dis,calc.children[1]);
+let display = document.querySelector('.display');
+
+function createNumber(strNumber,num){
+    strNumber = strNumber+num;
+    parseNumber = parseFloat(strNumber);
+    display.textContent = parseNumber;
+    return strNumber;
+}
+
+let one = document.querySelector('.one');
+one.addEventListener('click',() => strNumber = createNumber(strNumber,'1'));
+
+let two = document.querySelector('.two');
+two.addEventListener('click',() => strNumber = createNumber(strNumber,'2'));
+
+let three = document.querySelector('.three');
+three.addEventListener('click',() => strNumber = createNumber(strNumber,'3'));
+
+let four = document.querySelector('.four');
+four.addEventListener('click',() => strNumber = createNumber(strNumber,'4'));
+
+let five = document.querySelector('.five');
+five.addEventListener('click',() => strNumber = createNumber(strNumber,'5'));
+
+let six = document.querySelector('.six');
+six.addEventListener('click',() => strNumber = createNumber(strNumber,'6'));
+
+let seven = document.querySelector('.seven');
+seven.addEventListener('click',() => strNumber = createNumber(strNumber,'7'));
+
+let eight = document.querySelector('.eight');
+eight.addEventListener('click',() => strNumber = createNumber(strNumber,'8'));
+
+let nine = document.querySelector('.nine');
+nine.addEventListener('click',() => strNumber = createNumber(strNumber,'9'));
+
+let zero = document.querySelector('.zero');
+zero.addEventListener('click',() => strNumber = createNumber(strNumber,'0'));
+
+let clear = document.querySelector('.clear');
+clear.addEventListener('click', () =>{
+    strNumber = '';
+    number = 0;
+    display.textContent = number;
+})
+let operator;
+
+let multiple = document.querySelector('.multiply');
+multiple.addEventListener('click',() =>{
+    if(firstNumber== ''){
+        firstNumber = parseNumber;
+        operator = '*';
+        display.textContent = parseNumber+ ' ' + operator;
+        strNumber = '';
+        parseNumber = 0;
+    }
+    else{
+        secondNumber = parseNumber;
+    }
+})
+
 const operators = ['+' ,'-', '*', '/'];
 
 function operate(){
